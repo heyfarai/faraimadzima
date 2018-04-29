0d8d9b8c0fc04bd6ec119709869bf908628cd666
Farai Madzima <farai@me.com>
2018-04-29 18:29:37 -0400

    add proegressive jpg

--- static/serviceworker.js
+++ static/serviceworker.js
@@ -3,7 +3,7 @@
 // Import Jake's polyfill for async waitUntil
 importScripts('/js/async-waituntil.js');
 
-const version = 'v0.10::';
+const version = 'v0.11::';
 const staticCacheName = version + 'static';
 
 function updateStaticCache() {
